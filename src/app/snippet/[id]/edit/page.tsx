ab5b513b9457fdbb982735b2d76e3b4e8e92784b
SuchitHubale <2217043@ritindia.edu>
2025-07-08 13:09:39 +0530

    fix deployment error

--- src/app/snippet/[id]/edit/page.tsx
+++ src/app/snippet/[id]/edit/page.tsx
@@ -3,18 +3,31 @@ import { db } from '@/lib/firebase';
 import React from 'react'
 import { doc, getDoc } from 'firebase/firestore';
 
+type Snippet = {
+  id: string;
+  title: string;
+  code: string;
+};
+
 const EditPageSnippets = async({params}:{params:Promise<{id:string}>}) => {
     const id = (await params).id;
     const snippetDoc = await getDoc(doc(db, "snippets", id));
-    const snippet = snippetDoc.exists() ? { id: snippetDoc.id, ...snippetDoc.data() } : null;
+    const data = snippetDoc.data() as Partial<Omit<Snippet, "id">> | undefined;
+    const snippet: Snippet | null = snippetDoc.exists()
+      ? {
+          id: snippetDoc.id,
+          title: data?.title ?? "(Untitled)",
+          code: data?.code ?? "",
+        }
+      : null;
 
     if(!snippet){
         return <h1>Snippet not found</h1>
     }
 
-  return (
-    <EditSnippetForm snippet = {snippet}/>
-  )
+    return (
+      <EditSnippetForm snippet={snippet}/>
+    )
 }
 
 export default EditPageSnippets
