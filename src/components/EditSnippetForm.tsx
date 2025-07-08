53dc120b88b658157b00fa766461a11bb476d644
SuchitHubale <2217043@ritindia.edu>
2025-07-08 12:45:16 +0530

    fix deployment error

--- src/components/EditSnippetForm.tsx
+++ src/components/EditSnippetForm.tsx
@@ -1,9 +1,14 @@
 "use client";
 import React, { useState } from 'react'
 import { Editor } from '@monaco-editor/react'
-import type { Snippet } from '@prisma/client'
 import { Button } from './ui/button';
 import SaveSnippet from '@/actions'
+
+type Snippet = {
+  id: string;
+  title: string;
+  code: string;
+};
 
 const EditSnippetForm = ({ snippet }: { snippet: Snippet }) => {
 
